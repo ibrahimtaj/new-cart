96ebb2848006bdfc6ab4314dac55c47e46780787
ibrahimtaj <taj_it@yahoo.com>
2020-05-04 13:01:32 +0530

    Updates

---
+++ precache-manifest.0ff641d4bed951fc7f3bbc7fce38918e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e69b33f3ab29b491a27105dfc000ac6",
+    "url": "/new-cart/index.html"
+  },
+  {
+    "revision": "54b8fb075199fcdfdf8b",
+    "url": "/new-cart/static/css/main.63bf6e56.chunk.css"
+  },
+  {
+    "revision": "613e2f33babafcc2e0c3",
+    "url": "/new-cart/static/js/2.1cfb772d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new-cart/static/js/2.1cfb772d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b8fb075199fcdfdf8b",
+    "url": "/new-cart/static/js/main.b7d49541.chunk.js"
+  },
+  {
+    "revision": "a550345d76abb7d2297d",
+    "url": "/new-cart/static/js/runtime-main.dd360df2.js"
+  },
+  {
+    "revision": "02ed288f8e38d26052e622bf7174559f",
+    "url": "/new-cart/static/media/laptop.02ed288f.jpg"
+  }
+]);
